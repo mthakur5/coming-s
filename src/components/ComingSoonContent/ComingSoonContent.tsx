83b88e871b2ac373c2efc96d685a684a4c1319db
Manmohan Kumar <manmohankumar@Manmohans-MacBook-Pro.local>
2024-08-11 17:21:35 +0530

    commit-2

--- src/components/ComingSoonContent/ComingSoonContent.tsx
+++ src/components/ComingSoonContent/ComingSoonContent.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import data from "../../../data/data";
 import Image from "next/image";
+import Link from "next/link";
 
 function ComingSoonContent() {
   const { description, title } = data;
+
   return (
     <>
-     <Image src="/logo.png" alt="Haroth Group Logo" width={350} height={150} />
-      <div className="relative flex flex-col  place-items-center ">
-        <h2 className="text-center font-heading m-10 text-6xl sm:text-7xl lg:text-8xl leading-[5rem] sm:leading-[7rem] lg:leading-[7rem] font-black	 ">
+      <Image src="/logo.png" alt="Haroth Group Logo" width={350} height={150} />
+      <div className="relative flex flex-col place-items-center ">
+        <h2 className="text-center font-heading m-10 text-6xl sm:text-7xl lg:text-8xl leading-[5rem] sm:leading-[7rem] lg:leading-[7rem] font-black">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
             {title}
           </span>
@@ -20,6 +22,13 @@ function ComingSoonContent() {
             __html: description,
           }}
         ></p>
+
+        {/* Button to redirect to furniture.haroth.com */}
+        <Link href="https://furniture.haroth.com" passHref>
+          <button className="mt-8 px-6 py-3 text-lg font-semibold text-white bg-blue-500 rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition duration-200 ease-in-out">
+            Visit Our Furniture Store
+          </button>
+        </Link>
       </div>
     </>
   );
